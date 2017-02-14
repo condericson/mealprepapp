4dcdebd405c0ad905d3a735c026e3c1f7d95a836
condericson@gmail.com <condericson@gmail.com>
2017-02-13 19:54:19 -0500

    working on REST

---
+++ javascript/REST.js
@@ -0,0 +1,117 @@
+$(document).ready(function() {
+
+
+const express = require('express');
+const app = express();
+const {BasicStrategy} = require('passport-http');
+const jsonParser = require('body-parser').json();
+
+const {User} = require('./userModel');
+
+const router = express.Router();
+
+router.use(jsonParser);
+
+//Creating user with username and password
+router.post('/', (req, res) => {
+  if (!req.body) {
+    return res.status(400).json({message: 'No request body'});
+  }
+
+  if (!('username' in req.body)) {
+    return res.status(422).json({message: 'Missing field: username'});
+  }
+
+  let {username, password, chefName} = req.body;
+
+  if (typeof username !== 'string') {
+    return res.status(422).json({message: 'Incorrect field type: username'});
+  }
+
+  username = username.trim();
+
+  if (username === '') {
+    return res.status(422).json({message: 'Incorrect field length: username'});
+  }
+
+  if (!(password)) {
+    return res.status(422).json({message: 'Missing field: password'});
+  }
+
+  if (typeof password !== 'string') {
+    return res.status(422).json({message: 'Incorrect field type: password'});
+  }
+
+  password = password.trim();
+
+  if (password === '') {
+    return res.status(422).json({message: 'Incorrect field length: password'});
+  }
+
+  // check for existing user
+  return User
+    .find({username})
+    .count()
+    .exec()
+    .then(count => {
+      if (count > 0) {
+        return res.status(422).json({message: 'username already taken'});
+      }
+      // if no existing user, hash password
+      return User.hashPassword(password)
+    })
+    .then(hash => {
+      return User
+        .create({
+          username: username,
+          password: hash,
+          chefName: firstName
+        })
+    })
+    .then(user => {
+      return res.status(201).json(user.apiRepr());
+    })
+    .catch(err => {
+      res.status(500).json({message: 'Internal server error'})
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Logging in with existing username and password
+$('#js-login').submit(function(event){
+	console.log("Logging in...");
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
---
+++ javascript/userModel.js
@@ -0,0 +1,38 @@
+const bcrypt = require('bcryptjs');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+const STATE_ABBREVIATIONS = Object.keys(require('./state-abbreviations'));
+
+const UserSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  chefName: {type: String, default: ""}
+});
+
+UserSchema.methods.apiRepr = function() {
+  return {
+    username: this.username || '',
+    chefName: this.lastName || ''
+  };
+}
+
+UserSchema.methods.validatePassword = function(password) {
+  return bcrypt.compare(password, this.password);
+}
+
+UserSchema.statics.hashPassword = function(password) {
+  return bcrypt.hash(password, 10);
+}
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = {User};
