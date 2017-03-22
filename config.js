05fb266e7765c18e69854cf9521f6e104cbd751d
condericson@gmail.com <condericson@gmail.com>
2017-03-22 10:18:17 -0400

    fixing config.js

--- config.js
+++ config.js
@@ -5,11 +5,3 @@ exports.TEST_DATABASE_URL = (
 	process.env.TEST_DATABASE_URL ||
 	'mongodb://localhost/test-meal-prep-app');
 exports.PORT = process.env.PORT || 8080;
-
-//
-// exports.DATABASE_URL = process.env.DATABASE_URL ||
-//                        global.DATABASE_URL ||
-//                        (process.env.NODE_ENV === 'production' ?
-//                             'mongodb://<dbUsername>:<dbPassword>@ds049538.mlab.com:49538/simply_todos' :
-//                             'mongodb://localhost/simply-to-dos');
-// exports.PORT = process.env.PORT || 8080;
