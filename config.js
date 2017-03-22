9b3e30d422123d2104da7c09523fa1f314ee8f52
condericson@gmail.com <condericson@gmail.com>
2017-03-22 10:13:18 -0400

    fixing config.js

--- config.js
+++ config.js
@@ -1,6 +1,15 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                      (process.env.NODE_ENV === 'production' ?
-                           'mongodb://condericson:12345@ds041536.mlab.com:41536/heroku_13dqhzp9' :
-                           'mongodb://localhost/meal-prep-app');
+                      'mongodb://localhost/meal-prep-app';
+exports.TEST_DATABASE_URL = (
+	process.env.TEST_DATABASE_URL ||
+	'mongodb://localhost/test-meal-prep-app');
 exports.PORT = process.env.PORT || 8080;
+
+//
+// exports.DATABASE_URL = process.env.DATABASE_URL ||
+//                        global.DATABASE_URL ||
+//                        (process.env.NODE_ENV === 'production' ?
+//                             'mongodb://<dbUsername>:<dbPassword>@ds049538.mlab.com:49538/simply_todos' :
+//                             'mongodb://localhost/simply-to-dos');
+// exports.PORT = process.env.PORT || 8080;
