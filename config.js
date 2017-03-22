22633f16306f8ddb88ed8a766710163b68e21206
condericson@gmail.com <condericson@gmail.com>
2017-03-22 10:40:01 -0400

    fixing heroku

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                       'mongodb://localhost/meal-prep-app';
+                      
 exports.TEST_DATABASE_URL = (
 	process.env.TEST_DATABASE_URL ||
 	'mongodb://localhost/test-meal-prep-app');
